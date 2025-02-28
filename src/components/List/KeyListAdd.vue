88a39e5a84f1561e9444249e315ec05ffb0798ac
Koras02 <wjdgh0727@gmail.com>
2025-02-28 09:28:58 +0900

    2.리스트와 키

---
+++ src/components/List/KeyListAdd.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+    </ul>
+    <button @click="addItem">Add Item</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { id: 1, name: 'item 1' },
+        { id: 2, name: 'item 2' },
+      ],
+    }
+  },
+  methods: {
+    addItem() {
+      const newId = this.items.length + 1
+      this.items.push({ id: newId, name: `item ${newId}` })
+    },
+  },
+}
+</script>
---
+++ src/components/List/KeyValue.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li v-for="item in items">{{ item.name }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { id: 1, name: 'item1' },
+        { id: 2, name: 'item2' },
+        { id: 3, name: 'item3' },
+      ],
+    }
+  },
+}
+</script>
